3898aa5d6e0caea3845781acf69f76539fdb9c99
kmpape <kempf.idris@gmail.com>
2022-03-18 10:10:22 +0100

    Added auto-generated files for GSVD control

--- imc/IMC_transfer.h
+++ imc/IMC_transfer.h
@@ -1,6 +1,9 @@
 #ifndef IMC_IMC_TRANSFER_H_
 #define IMC_IMC_TRANSFER_H_
 
+#include "utils/libQDMA.h"
+#include "imc/IMC_DI_ctr.h"
+
 int BPM_to_float(const LIBQDMA_ARR_TYPE * in_vec, imc_float * out_vec);
 int CM_to_int(const imc_float * in_vec, LIBQDMA_ARR_TYPE * out_vec);
 
