a0fc26b0da7b7968c6b7e8b37e68d4fa7a89827c
Idris Kempf <etv24172@cs05r-sc-serv-08.diamond.ac.uk>
2022-08-02 21:09:07 +0100

    Code that goes with v1.0.2-beta

--- fofb_config.h
+++ fofb_config.h
@@ -11,8 +11,8 @@
 /*
  * Control Algorithms
  */
-#define IMC_CONTROL         (0) // enable=1, set to 0 if loopback only
-#define GSVD_CONTROL        (1) // enable=1, set to 0 if loopback only
+#define IMC_CONTROL         (1) // enable=1, set to 0 if loopback only
+#define GSVD_CONTROL        (0) // enable=1, set to 0 if loopback only
 #define MPC_CONTROL         (0) // enable=1, set to 0 if loopback only
 #define UNIT_TEST           (0) // run unit test loop instead of real-time control
 #define INTEGRATOR_LIMIT    (4.99) // in Amperes
